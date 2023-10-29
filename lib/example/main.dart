6c642d90031645645e7451f38abc194a7d981436
Mashood.H <mashoodhussain100@gmail.com>
2023-10-30 04:03:31 +0500

    Update main.dart

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -16,25 +16,45 @@ class MyApp extends StatelessWidget {
       'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
       'https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
     ];
-
+    List<String> name = [
+      "John Clark",
+      "Mark Medok",
+      "Berry allen",
+      "Diana Smith",
+      "Alexa Dean"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Overlapping Avatars'),
         ),
-        body: Center(
-          child: OverLappingNetworkAvatarBuilder(
-            isTotalMemberVisible: true,
-            listOfNetworkImages: assetImages,
-            limitAvatars: 3,
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            circularAvatarColor: Colors.red,
-            circularAvatarSize: 50,
-            distanceInEachAvatar: 60,
-            totalMembersHintTextColor: Colors.white,
-            totalMembersHintTextSize: 18,
-          ),
+        body: Column(
+          children: [
+            OverLappingNetworkAvatarBuilder(
+              isTotalMemberVisible: true,
+              listOfNetworkImages: assetImages,
+              limitAvatars: 3,
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.center,
+              circularAvatarColor: Colors.red,
+              circularAvatarSize: 50,
+              distanceInEachAvatar: 60,
+              totalMembersHintTextColor: Colors.white,
+              totalMembersHintTextSize: 18,
+            ),
+            OverLappingTextAvatarBuilder(
+              listOFAvatarName: name,
+              isTotalMemberVisible: true,
+              limitAvatars: 3,
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.center,
+              circularAvatarColor: Colors.red,
+              circularAvatarSize: 50,
+              distanceInEachAvatar: 60,
+              totalMembersHintTextColor: Colors.white,
+              totalMembersHintTextSize: 18,
+            ),
+          ],
         ),
       ),
     );
